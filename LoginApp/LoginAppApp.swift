b3670d9fb6a073e36bfd3ee06b2148bb93fa9c73
Dethie DIOUF <55641058+DETHIEDIOUF4@users.noreply.github.com>
2022-03-08 15:59:10 +0000

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Dethie DIOUF on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
